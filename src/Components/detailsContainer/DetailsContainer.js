d3741d7dee9e79813e78abc8132168efe57cf115
notstoe <gstonin95@gmail.com>
2021-02-03 19:03:05 +0000

    units circle, styling

--- src/Components/detailsContainer/DetailsContainer.js
+++ src/Components/detailsContainer/DetailsContainer.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./DetailsContainer.css";
 
 function DetailsContainer() {
-	return <div className="detailsContainer"></div>;
+	return (
+		<div className="detailsContainer">
+			<div className="unitsContainer">
+				<p className="unitsCircle">°C</p>
+				<p className="unitsCircle">°F</p>
+			</div>
+		</div>
+	);
 }
 
 export default DetailsContainer;
