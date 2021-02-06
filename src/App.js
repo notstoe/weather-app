5d3d8c089518bb53de931bd8e5f00d5032258c1b
notstoe <gstonin95@gmail.com>
2021-02-06 01:20:56 +0000

    state object reorganized

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ForecastContainer from "./Components/ForecastContainer/ForecastContainer"
 function App() {
 	const [weatherData, setWeatherData] = useState({ loading: true });
 	useEffect(() => {
+		//useEffect as ComponentDidMount
 		fetch(
 			"http://api.openweathermap.org/data/2.5/weather?q=Liverpool&units=metric&appid=6bf8b98d56a02598af5baf4525e45b8a"
 		)
@@ -15,11 +16,12 @@ function App() {
 				setWeatherData({
 					loading: false,
 					name: data.name,
-					feels_like: data.main.feels_like,
 					temp: data.main.temp,
+					feels_like: data.main.feels_like,
 					temp_max: data.main.temp_max,
 					temp_min: data.main.temp_min,
 					humidity: data.main.humidity,
+					visibility: data.visibility,
 					pressure: data.main.pressure,
 					overall: data.weather[0].description,
 				});
@@ -28,8 +30,7 @@ function App() {
 
 	return (
 		<div className="pageContainer">
-			{/* <p>{state.loading ? "loading..." : state.temp + " ℃"}</p> */}
-			<ForecastContainer />
+			<ForecastContainer weatherData={weatherData} />
 			<DetailsContainer />
 		</div>
 	);
